9923f48f53f732647d2aa6893577a9c512428b4e
sarahsitis <sarahsitis@gmail.com>
2019-11-20 10:34:18 +0700

    rename contact.php to send.php, add ini_set and error_reporting on send.php

--- send.php.php
+++ send.php.php
@@ -1,15 +1,18 @@
 <?php 
 if(isset($_POST['send'])){
+	ini_set( 'display_errors',1);
+	error_reporting( E_ALL );
+
 	$admin = "pure.cianjur@gmail.com";
 	$name = htmlentities($_POST['name']);
 	$email = htmlentities($_POST['email']);
 	$subject = 'Cust Message';
 	$message = htmlentities($_POST['message']);
 
-	$sender = 'Dari:'.$name.' <'.$email.'>';
+	$sender = 'From:'.$name.' <'.$email.'>';
 
 	if(mail($admin, $subject, $message, $sender)){
-		echo "<script>alert('Message sent!');</script>";
+		echo "<script>alert('Message sent!')</script>";
 		header("Location:index.html");
 	}
 
